b01b7c00e221ce2bf7b37b5f83b4db2853b8b2d1
Tiago Souza <tgsouza86@gmail.com>
2023-11-11 14:18:30 -0300

    Comandos

--- cypress/integration/barriga/functional.spec.js
+++ cypress/integration/barriga/functional.spec.js
@@ -1,6 +1,7 @@
 ///<reference types="cypress" />
 
 import loc from '../../support/locators'
+import '../../support/commandsContas'
 
 describe('Should Test at a functional level ', () => {
    
@@ -15,10 +16,15 @@ describe('Should Test at a functional level ', () => {
     })
 
     it('Should creater an account', () => {
-        cy.get(loc.MENU.SETTINGS).click()
-        cy.get(loc.MENU.CONTAS).click()
-        cy.get(loc.CONTAS.NOME).type('Conta de teste Tiago')
-        cy.get(loc.CONTAS.BTN_SALVAR).click()
+        cy.acessarMenuConta()
+       cy.inserirConta()
+      //  cy.get(loc.MENU.SETTINGS).click()
+      //  cy.get(loc.MENU.CONTAS).click()
+
+
+
+     //   cy.get(loc.CONTAS.NOME).type('Conta de teste Tiago')
+     //   cy.get(loc.CONTAS.BTN_SALVAR).click()
         cy.get(loc.MESSAGE).should('contain', 'Conta inserida com sucesso')
     })
 
@@ -26,8 +32,9 @@ describe('Should Test at a functional level ', () => {
 
     it('Should update an account', () => {
        // cy.get(':nth-child(3) > :nth-child(2) > .fa-edit')
-       cy.get(loc.MENU.SETTINGS).click()
-       cy.get(loc.MENU.CONTAS).click()
+       cy.acessarMenuConta()
+     //  cy.get(loc.MENU.SETTINGS).click()
+      // cy.get(loc.MENU.CONTAS).click()
        cy.xpath(loc.CONTAS.XP_BTN_ALTERAR).click()
        cy.get(loc.CONTAS.NOME)
         .clear()
