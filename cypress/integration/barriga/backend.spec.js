3c4421c4fc85021b893c2432dc12b9eb52c48004
Tiago Souza <tgsouza86@gmail.com>
2023-11-14 13:56:55 -0300

    Login api

---
+++ cypress/integration/barriga/backend.spec.js
@@ -0,0 +1,61 @@
+///<reference types="cypress" />
+
+
+
+
+describe('Should Test at a functional level ', () => {
+   
+    before(() => {
+   //     cy.login('tgsouza89@gmail.com', 'ti01de02')
+    //    cy.resetApp()
+      //  cy.visit('https://barrigareact.wcaquino.me/') 
+      //  cy.get(loc.LOGIN.USER).type('tgsouza89@gmail.com')
+      //  cy.get(loc.LOGIN.PASSWORD).type('ti01de02')
+      //  cy.get(loc.LOGIN.BTN_LOGIN).click()
+      //  cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
+    })
+
+    it('Should creater an account', () => {
+       cy.request({
+            method: 'POST',
+            url: 'https://barrigarest.wcaquino.me/signin',
+            body: {
+                email: "tgsouza89@gmail.com",
+                redirecionar: false,
+                senha:"ti01de02"
+            }
+
+       }).its('body.token').should('not.be.empty')
+        
+    })
+
+
+
+    it('Should update an account', () => {
+       // cy.get(':nth-child(3) > :nth-child(2) > .fa-edit')
+       
+    })
+
+
+    it('Should not create an account with same name', () =>{
+
+
+    })
+
+
+
+    it('Should create a transaction', () =>{
+       
+
+    })
+    it('Should get balace', () =>{
+  
+    })
+    it('Should remova a transaction', () =>{
+      
+            
+
+    })
+})
+
+
