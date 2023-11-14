201da6fb0bcfcdd4597d16f5639459b8a6aacf5a
Tiago Souza <tgsouza86@gmail.com>
2023-11-14 15:46:54 -0300

    Comands

--- cypress/integration/barriga/backend.spec.js
+++ cypress/integration/barriga/backend.spec.js
@@ -4,40 +4,35 @@
 
 
 describe('Should Test at a functional level ', () => {
-   
+   let token
     before(() => {
-   //     cy.login('tgsouza89@gmail.com', 'ti01de02')
-    //    cy.resetApp()
-      //  cy.visit('https://barrigareact.wcaquino.me/') 
-      //  cy.get(loc.LOGIN.USER).type('tgsouza89@gmail.com')
-      //  cy.get(loc.LOGIN.PASSWORD).type('ti01de02')
-      //  cy.get(loc.LOGIN.BTN_LOGIN).click()
-      //  cy.get(loc.MESSAGE).should('contain', 'Bem vindo')
+        cy.getToken('tgsouza89@gmail.com', 'ti01de02')
+        .then(tkn => {
+            token = tkn
+        })
+
+  
     })
 
     it('Should creater an account', () => {
-      cy.request({
-           method: 'POST',
-           url: 'https://barrigarest.wcaquino.me/signin',
-           body: {
-               email: "tgsouza89@gmail.com",
-               redirecionar: false,
-                senha:"ti01de02"
-           }
-
-       }).its('body.token').should('not.be.empty')
-       .then(token => {
+      
+     
         cy.request({
             url: 'https://barrigarest.wcaquino.me/contas',
             method: 'POST',
             headers:{Authorization: `JWT ${token}`},
             body: {
-                nome: 'Conta via rest'   
+                nome: 'Conta via resttt'   
     
             }
-          }). then(res => console.log(res))
+          }).as('response')
         
-       }) 
+      
+       cy.get('@response').then(res => {
+            expect(res.status).to.be.equal(201)
+            expect(res.body).to.have.property('id')
+            expect(res.body).to.have.property('nome', 'Conta via resttt')
+       })
         
     })
 
